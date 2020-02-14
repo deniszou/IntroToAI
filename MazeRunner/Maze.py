c2aac397af08a608bf6283352957db25e60052b6
Jim Tang <jimruitang@gmail.com>
2020-02-14 18:38:30 -0500

    Created Maze class and created Runner class. Run Runner.py to test

---
+++ MazeRunner/Maze.py
@@ -0,0 +1,18 @@
+import os
+import numpy
+
+
+class Maze():
+    def __init__(self, dim, prob):
+        self.dim = dim
+        self.prob = prob
+
+        self.nums = numpy.random.choice(['F', 'E'], size=(dim, dim), p=[prob, 1 - prob])
+        self.nums[0, 0] = 'S'
+        self.nums[dim - 1, dim - 1] = 'G'
+
+    def printMaze(self):
+        for x in range(self.dim):
+            print("\n")
+            for y in range(self.dim):
+                print(self.nums[x][y], end="      ")
