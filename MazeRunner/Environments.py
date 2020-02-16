c3c2ca61605fe2e50809cb59170350e0ca654dff
Denis Zou <deniszou@gmail.com>
2020-02-15 19:38:55 -0500

    minor errors fixed

--- MazeRunner/Environments.py
+++ MazeRunner/Environments.py
@@ -6,6 +6,7 @@ def createMaze(dim, prob):
     nums = numpy.random.choice(['F', 'E'], size=(dim, dim), p=[prob, 1 - prob])
     nums[0, 0] = 'S'
     nums[dim - 1, dim - 1] = 'G'
+    return nums
 
 def dfs(maze):
     visited = {}
@@ -21,6 +22,7 @@ def dfs(maze):
             getNeighbors(maze, node[0], node[1], visited, stack, path)
         i += 1
 
+
 def isValid(maze, x, y):
     if x < 0 or y < 0 or (x > maze.shape[0] - 1) or y > (maze.shape[1] - 1):
         return False
@@ -28,21 +30,36 @@ def isValid(maze, x, y):
         return False
     return True
 
+
 def getNeighbors(maze, x, y, visited, stack, pathO):
     if isValid(maze, x - 1, y) and (x - 1, y) not in visited:
         path1 = pathO.copy()
-        path1.append(x - 1, y)
+        path1.append((x - 1, y))
         stack.append(path1)
     if isValid(maze, x, y - 1) and (x, y - 1) not in visited:
         path2 = pathO.copy()
-        path2.append(x, y - 1)
+        path2.append((x, y - 1))
         stack.append(path2)
     if isValid(maze, x + 1, y) and (x + 1, y) not in visited:
         path3 = pathO.copy()
-        path3.append(x + 1, y)
+        path3.append((x + 1, y))
         stack.append(path3)
     if isValid(maze, x, y + 1) and (x, y + 1) not in visited:
         path4 = pathO.copy()
-        path4.append(x, y + 1)
+        path4.append((x, y + 1))
         stack.append(path4)
+    return stack
+
+
+def printMaze(self):
+    for x in range(self.shape[0]):
+        print("\n")
+        for y in range(self.shape[1]):
+            print(self[x][y], end=" ")
+
+
+maze = createMaze(10, 0.3)
+print(dfs(maze))
+printMaze(maze)
+print(dfs(maze))
 
