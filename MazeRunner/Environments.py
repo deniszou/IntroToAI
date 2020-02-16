4d3ae72cc03bc468c13260abb4c2603b3538d7d2
Jim Tang <jimruitang@gmail.com>
2020-02-15 19:39:47 -0500

    merge conflicts

--- MazeRunner/Environments.py
+++ MazeRunner/Environments.py
@@ -2,49 +2,7 @@ import os
 import numpy
 
 
-# depth first uses stack
-def dfs(maze, currX, currY, visited, paths, stack):
-    visited.append(currX, currY)
-    if maze[currX, currY] == 'G':
-        return visited
-
-
-def checkNeighbors(maze, x, y, visited, stack):
-    if edgeCheck(maze, x, y) == "top":
-        if edgeCheck(maze, x, y) == "right":
-            stack.push((x + 1, y))
-            stack.push((x, y - 1))
-
-
-def edgeCheck(maze, x, y):
-    if x == 0:
-        return "top"
-    if x == maze.shape[0]:
-        return "bottom"
-    if y == 0:
-        return "left"
-    if y == maze.shape[1]:
-        return "right"
-
-
-#-----------A Star-----------
-def getEuclid(self, dim, x, y):
-    a = (dim - 1 - x) ** 2
-    b = (dim - 1 - y) ** 2
-    return numpy.sqrt(a + b)
-
-
-def getManhattan(self, dim, x, y):
-    a = abs(dim - 1 - x)
-    b = abs(dim - 1 - y)
-    return a + b
-
-
-def aStar(maze, self):
-    dim = maze.dim
-
-
-#-----------DFS-----------
+# -----------DFS-----------
 def dfs(maze):
     visited = {}
     stack = [[(0, 0)]]
@@ -53,10 +11,10 @@ def dfs(maze):
         path = stack.pop()
         node = path.pop()
         visited[i] = node
-        if maze[node] == 'G':
+        if maze.nums[node] == 'G':
             return path
         else:
-            getNeighbors(maze, node[0], node[1], visited, stack, path)
+            getNeighbors(maze.nums, node[0], node[1], visited, stack, path)
         i += 1
 
 
@@ -85,3 +43,67 @@ def getNeighbors(maze, x, y, visited, stack, pathO):
         path4 = pathO.copy()
         path4.append(x, y + 1)
         stack.append(path4)
+
+
+# -----------A Star-----------
+def getEuclid(self, dim, x, y):
+    a = (dim - 1 - x) ** 2
+    b = (dim - 1 - y) ** 2
+    return numpy.sqrt(a + b)
+
+
+def getManhattan(self, dim, x, y):
+    a = abs(dim - 1 - x)
+    b = abs(dim - 1 - y)
+    return a + b
+
+
+def aStar(maze, self):
+    dim = maze.dim
+    visited = {}
+
+    # (x,x,x) represents (x coord, y coord, heuristic)
+    # sortedList is sorted from highest to lowest heuristic
+    sortedList = [[(0, 0, 0)]]
+    i = 0
+    while sortedList:
+        path = sortedList.pop()
+        node = path.pop()
+        visited[i] = node
+        if maze[node] == 'G':
+            return path
+        else:
+            getNeighborsAStar(maze, node[0], node[1], visited, sortedList, path, dim)
+            sortedList = sortByHeuristic(sortedList)
+        i += 1
+
+
+def getNeighborsAStar(maze, x, y, visited, sortedList, pathO, dim):
+    if isValid(maze, x - 1, y) and (x - 1, y) not in visited:
+        path1 = pathO.copy()
+        path1.append(x - 1, y, getEuclid(dim, x - 1, y))
+        sortedList.append(path1)
+    if isValid(maze, x, y - 1) and (x, y - 1) not in visited:
+        path2 = pathO.copy()
+        path2.append(x, y - 1, getEuclid(dim, x, y - 1))
+        sortedList.append(path2)
+    if isValid(maze, x + 1, y) and (x + 1, y) not in visited:
+        path3 = pathO.copy()
+        path3.append(x + 1, y, getEuclid(dim, x + 1, y))
+        sortedList.append(path3)
+    if isValid(maze, x, y + 1) and (x, y + 1) not in visited:
+        path4 = pathO.copy()
+        path4.append(x, y + 1, getEuclid(dim, x, y + 1))
+        sortedList.append(path4)
+
+
+def sortByHeuristic(sortedList):
+    l = len(sortedList)
+    for i in range(0,l):
+        for j in range(0, l-i-1):
+            if sortedList[j][i] > sortedList[j + 1][1]:
+                tempo = sortedList[j]
+                sortedList[j] = sortedList[j+1]
+                sortedList[j+1] = tempo
+    return sortedList
+
--- MazeRunner/Runner.py
+++ MazeRunner/Runner.py
@@ -3,9 +3,10 @@ import Environments
 
 if __name__ == '__main__':
     # ---------------Create & print maze---------------
-    dimension = int(input("What is the demnsion of the maze?\n"))
-    percent = float(input("What is the percentage chance a space is filled?\n"))
+    dimension = int(input("What is the demnsion of the maze (Enter whole number)?\n"))
+    percent = float(input("What is the percentage chance a space is filled (Enter between 0 and 1)?\n"))
     maze = Maze(dimension, percent)
     maze.printMaze()
 
     #---------------Print out solutions here---------------
+    Environments.dfs(maze)
