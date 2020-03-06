b9d439954ea5425f9d0fa8c564d584ed37fb87b4
Denis Zou <deniszou@gmail.com>
2020-03-06 18:22:21 -0500

    agentcell class

---
+++ MineSweeper/AgentCell.py
@@ -0,0 +1,8 @@
+class AgentCell:
+    def __init__(self):
+        # 'c' for covered, 's' for safe 'm' for mine
+        self.status = 'c'
+        self.cluemines
+        self.surrsafe
+        self.surrmines
+        self.surrhidden
