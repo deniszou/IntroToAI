c314b9601fa9bc65efd3197c600b2fbf6f6689b3
Denis Zou <deniszou@gmail.com>
2020-05-05 15:01:12 -0400

    knn, kmeans somewhat debugged

--- Colorization/BasicAgent.py
+++ Colorization/BasicAgent.py
@@ -6,7 +6,7 @@ def parsepixels(file):
     im = Image.open(file)
     width, height = im.size
     block = []
-    #k = 0
+    k = 0
     rmax = 0
     gmax = 0
     bmax = 0
@@ -14,8 +14,9 @@ def parsepixels(file):
     greyColorList = {}
     trueColorList = {}
     repColorList = {}
+    greyNeighbors = []
     #create list of 3x3
-    #create list of rgb
+    #create list of rgb, greyscale, repcolor
     #iterate through pixels
     for x in range(1, width - 1):
         for y in range(1, height - 1):
@@ -27,39 +28,41 @@ def parsepixels(file):
             trueColorList[x, y] = im.getpixel(x,y)
             greyColorList[x, y] = trueColorList[x, y][0] * 0.21 + trueColorList[x, y][1] * 0.72 + trueColorList[x, y][2] * 0.07
 
-            if trueColorList[k][0] > rmax:
-                rmax = trueColorList[k][0]
-            if trueColorList[k][1] > gmax:
-                gmax = trueColorList[k][1]
-            if trueColorList[k][2] > bmax:
-                bmax = trueColorList[k][2]
+            #if trueColorList[k][0] > rmax:
+            #    rmax = trueColorList[k][0]
+            #if trueColorList[k][1] > gmax:
+            #    gmax = trueColorList[k][1]
+            #if trueColorList[k][2] > bmax:
+            #    bmax = trueColorList[k][2]
 
-            #k += 1
-    repColors = kmeans(trueColorList, width * height)
+            k += 1
+    repColors = kmeans(trueColorList, width, height)
     for x in range(0, width/2):
         for y in range(0, height):
+            # convert left side to rep colors
             repColorList[x, y] = findClosest(truecolorList[x, y], repColors)
-
-
+            # convert right side with knn
+            greyNeighbors = findNeighbors(greyColorList[x, y], greyColorList, width, height, 6)
 
 
 #run k = 5 clustering here
-def kmeans(list, listSize):
+def kmeans(list, width, height):
 
     #pick center indices with k = 5
-    c = np.random.randint(0, listSize, size=5)
+    c = numpy.random.randint(0, width, size=(5, 2))
 
     for i in range(1000):
         # create partitions(group clusters)
         output = []  #clusters
         ind = 0  #shud have looped over range instead
         check = 0  #breaks when centroids repeat
+        res = []
         mindist = (1000000, 0)
-        for point in list:
-            for center in c:
-                if point == center:
+        for point in list.values():
+            for centerInd in c:
+                if point == list[centerInd]:
                     continue
-                distance = eucd(point, center)
+                distance = eucd(point, list[centerInd])
                 if distance < mindist[0]:
                     mindist = (distance, ind)
                 ind += 1
@@ -68,11 +71,12 @@ def kmeans(list, listSize):
         #calculate true mean for each cluster
         for i in range(0, 5):
             mean = numpy.mean(output[i], axis=1)
-            if c[i] == mean:
+            if list[c[i]] == mean:
+                res[i] = list[c[i]]
                 check += 1
-            c[i] = mean
+            list[c[i]] = mean
         if check == 4:
-            return c
+            return res
             break
 
 
@@ -86,3 +90,20 @@ def findClosest(point, list):
         if eucd(point, list[i]) < min:
             min = (eucd(point, list[i]), i)
     return list[min[1]]
+
+
+#knn function
+def findNeighbors(point, list, width, height, n):
+    distList = []
+    xyList = {}
+    res = []
+    j = 0
+    for x in width/2:
+        for y in height:
+            xyList[eucd(point, list[x, y])] = (x, y)
+            distList[j] = eucd(point, list[x, y])
+            j += 1
+    distList.sort()
+    for i in range(0, n):
+        res.append(xyList.get(distList[i]))
+    return res
