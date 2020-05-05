4182bcac26e44fd0ee9812816f00c62c73897b83
Denis Zou <deniszou@gmail.com>
2020-05-05 00:55:52 -0400

    kmeans complete hasnt been debugged

--- Colorization/BasicAgent.py
+++ Colorization/BasicAgent.py
@@ -1,5 +1,6 @@
 import Image
 import numpy
+import math
 
 def parsepixels(file):
     im = Image.open(file)
@@ -13,8 +14,10 @@ def parsepixels(file):
     trueColorList = []
     #create list of 3x3
     #create list of rgb
+    #iterate through pixels
     for x in range(1, width - 1):
         for y in range(1, height - 1):
+            #iterate through 3x3 block
             for i in range(-1,2):
                 for j in range(-1,2):
                     block.append(x + i, y + j)
@@ -27,12 +30,41 @@ def parsepixels(file):
             if trueColorList[k][2] > bmax:
                 bmax = trueColorList[k][2]
             k += 1
-    repColors = kmeans(trueColorList, rmax, gmax, bmax, k)
+    repColors = kmeans(trueColorList, k)
 
 
 #run k = 5 clustering here
-def kmeans(list, rmax, gmax, bmax, listSize):
+def kmeans(list, listSize):
 
-    #pick center indices
+    #pick center indices with k = 5
     c = np.random.randint(0, listSize, size=5)
-    
+
+    for i in range(1000):
+        # create partitions(group clusters)
+        output = []  #clusters
+        ind = 0  #shud have looped over range instead
+        check = 0  #breaks when centroids repeat
+        mindist = (1000000, 0)
+        for point in list:
+            for center in c:
+                if point == center:
+                    continue
+                distance = eucd(point, center)
+                if distance < mindist[0]:
+                    mindist = (distance, ind)
+                ind += 1
+            output[mindist[1]].append(point)
+
+        #calculate true mean for each cluster
+        for i in range(0, 5):
+            mean = numpy.mean(output[i], axis=1)
+            if c[i] == mean:
+                check += 1
+            c[i] = mean
+        if check == 4:
+            return c
+            break
+
+
+def eucd(point1, point2):
+    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(point1, point2)]))
