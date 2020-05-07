d940efffd0c617965fbdb9a7426fa8b4792b02d6
Denis Zou <deniszou@gmail.com>
2020-05-07 01:26:01 -0400

    the final shitstained commit

--- Colorization/BasicAgent.py
+++ Colorization/BasicAgent.py
@@ -5,7 +5,7 @@ import math
 def parsepixels(file):
     im = Image.open(file)
     width, height = im.size
-    k = 0
+    #k = 0
     rmax = 0
     gmax = 0
     bmax = 0
@@ -14,10 +14,12 @@ def parsepixels(file):
     trueColorList = {}
     repColorList = {}
     testColorList = {}
+    #tlist = []
     #create list of 3x3
     #create list of rgb, greyscale, repcolor
     #iterate through pixels
     for x in range(0, width):
+        #tcol = []
         for y in range(0, height):
             block = []
             #iterate through 3x3 block
@@ -28,8 +30,9 @@ def parsepixels(file):
                 #pixelList is a list of 3x3 blocks
                 pixelList[x, y] = block
             trueColorList[x, y] = im.getpixel((x, y))
+            #tcol.append(im.getpixel((x, y)))
             greyColorList[x, y] = trueColorList[x, y][0] * 0.21 + trueColorList[x, y][1] * 0.72 + trueColorList[x, y][2] * 0.07
-
+        #tlist.append(tcol)
             #if trueColorList[k][0] > rmax:
             #    rmax = trueColorList[k][0]
             #if trueColorList[k][1] > gmax:
@@ -37,36 +40,80 @@ def parsepixels(file):
             #if trueColorList[k][2] > bmax:
             #    bmax = trueColorList[k][2]
 
-            k += 1
-    repColors = kmeans(trueColorList, width, height)
+            #k += 1
+    data = numpy.asarray(im)
+    #create grayscale image
+    gsList = []
+    for x in range(0, height):
+        col = []
+        for y in range(0, width):
+            col.append(int(greyColorList[y,x]))
+        gsList.append(col)
+    gsArray = numpy.asarray(gsList, dtype = 'uint8')
+    print('grey')
+    grayImage = Image.fromarray(gsArray, 'L')
+    grayImage.save('grey.png')
+
+    #testing data for output
+    csList = []
+    for x in range(0, height):
+        csList.append([])
+        for y in range(0, width):
+            csList[x].append(trueColorList[y,x])
+    csArray = numpy.asarray(csList, dtype = 'uint8')
+    print('color')
+
+    colorImage = Image.fromarray(csArray, 'RGB')
+    colorImage.save('color.png')
+
+    kmeansRes = kmeans(trueColorList, width, height)
+    repColors = kmeansRes[0]
+    clusters = kmeansRes[1]
+    cDict = kmeansRes[2]
 
     for x in range(1, width - 1):
         for y in range(1, height - 1):
             # convert to rep colors
             repColorList[x, y] = findClosest(trueColorList[x, y], repColors)
 
+    #output 5color image
+    repList = []
+    for x in range(1, height - 1):
+        col = []
+        for y in range(1, width - 1):
+            col.append(repColorList[y, x])
+        repList.append(col)
+    repArray = numpy.asarray(repList, dtype='uint8')
+    print('5color')
+    repImage = Image.fromarray(repArray, 'RGB')
+    repImage.save('5color.png')
+
     # convert right side with knn
-    ch = width/2
     for x in range(int(width/2), width - 1):
         for y in range(int(height/2), height - 1):
             greyBlock = []
             for pixel in pixelList[x, y]:
                 greyBlock.append(greyColorList[pixel])
-            testColorList[x, y] = findNeighbors(repColors, repColorList, pixelList, greyBlock, greyColorList, width, height, 6)
+            testColorList[x, y] = findNeighbors(x, y, cDict, clusters, repColors, repColorList, pixelList, greyBlock, greyColorList, width, height, 6)
 
     finalOutput = repColorList
     for x in range(int(width/2), width - 1):
         for y in range(int(height/2), height - 1):
             finalOutput[x, y] = testColorList[x, y]
 
-    grayImage = Image.fromarray(greyColorList)
-    grayImage.save('grey.png')
-    fiveColorImage = Image.fromarray(repColorList)
-    fiveColorImage.save('fiveColor.png')
-    finalImage = Image.fromarray(finalOutput)
-    finalImage.save('trainTest.png')
+    #final output here
+    finalList = []
+    for x in range(1, height - 1):
+        col = []
+        for y in range(1, width - 1):
+            col.append(finalList[y, x])
+        finalList.append(col)
+    finalArray = numpy.asarray(finalList, dtype='uint8')
+    print('final')
+    finalImage = Image.fromarray(finalArray, 'RGB')
+    finalImage.save('final.png')
 
-            #run k = 5 clustering here
+#run k = 5 clustering here
 def kmeans(list, width, height):
 
     #pick center indices with k = 5
@@ -75,33 +122,34 @@ def kmeans(list, width, height):
          (numpy.random.randint(0, width),numpy.random.randint(1, height)),
          (numpy.random.randint(0, width),numpy.random.randint(1, height)),
          (numpy.random.randint(0, width),numpy.random.randint(1, height))]
-#    for i in range(0, 5):
-#        for j in range(0, 2):
-#            if j == 0:
-#                c[i] = numpy.random.randint(0, width)
-#            else:
-#                c[i].append(numpy.random.randint(0, height))
+
     for i in range(1000):
         # create partitions(group clusters)
         output = [[],[],[],[],[]]  #clusters
         #ind = 0  #shud have looped over range instead
         check = 0  #breaks when centroids repeat
         res = [[],[],[],[],[]]
+        clusterDict = {}
+        xyClusters = [[],[],[],[],[]]
 
-        for point in list.values():
-            ind = 0
-            mindist = (1000000, 0)
-            for centerInd in c:
-                if point == list[centerInd[0], centerInd[1]]:
-                    continue
-                distance = eucd(point, list[centerInd[0], centerInd[1]])
-                #print(distance)
-                if distance < mindist[0]:
-                    mindist = (distance, ind)
-                ind += 1
-                #print(ind)
-            #print(mindist)
-            output[mindist[1]].append(point)
+        for x in range(0, width):
+            for y in range(0, height):
+                point = list[x, y]
+                ind = 0
+                mindist = (1000000, 0)
+                for centerInd in c:
+                    if point == list[centerInd[0], centerInd[1]]:
+                        continue
+                    distance = eucd(point, list[centerInd[0], centerInd[1]])
+                    # print(distance)
+                    if distance < mindist[0]:
+                        mindist = (distance, ind)
+                    ind += 1
+                    # print(ind)
+                # print(mindist)
+                output[mindist[1]].append(point)
+                xyClusters[mindist[1]].append((x, y))
+                clusterDict[x, y] = mindist[1]
         #print(mindist)
         #calculate true mean for each cluster
         for j in range(0, 5):
@@ -112,9 +160,9 @@ def kmeans(list, width, height):
                 check += 1
             list[c[j]] = mean
         if check == 5:
-            return res
+            return res, xyClusters, clusterDict
             break
-    return res
+    return res, xyClusters, clusterDict
 
 def eucd(point1, point2):
     return math.sqrt(sum([(a - b) ** 2 for a, b in zip(point1, point2)]))
@@ -129,18 +177,17 @@ def findClosest(point, list):
 
 
 #knn function greyblock holds grey values for 3x3 blocks on training side, compared against block, returns list of middle pixels
-def findNeighbors(repColors, repColorsList, blockList, block, alist, width, height, n):
+def findNeighbors(ex, why, cDict, clusters, repColors, repColorsList, blockList, block, alist, width, height, n):
     distList = []
-    #xyList = {}
     coords = []
     maxList = []
-    #eucList = []
     colorCount = [0, 0, 0, 0, 0]
     max = 0
-    #j = 0
 
-    for x in range(1, int(width/2)):
-        for y in range(1, height - 1):
+    for point in clusters[cDict[ex, why]]:
+        x = point[0]
+        y = point[1]
+        if x > 1 and x < width/2 and y > 1 and y < height - 1:
             greyBlock = []
             for pixel in blockList[x, y]:
                 greyBlock.append(alist[pixel])
